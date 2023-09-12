98ee33d0f9abb43bb6567475b7dc05c390bfc3e5
elescogna <eleonorascognamiglio03@gmail.com>
2023-09-12 17:03:26 -0400

    Completed task 4 from handout

---
+++ src/Reduce.java
@@ -0,0 +1,18 @@
+import static java.lang.Math.floorDiv;
+
+public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int n = 100;
+        while (n >= 1) {
+            if (n % 2 == 0) {
+                n = floorDiv(n, 2);
+            }
+            else {
+                n = n - 1;
+            }
+            count = count + 1;
+        }
+        System.out.println(count);
+    }
+}
